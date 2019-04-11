2571c69a6d6eaed98ae10f1d13c705ab6a0c3539
ThanhthanhQuy <38715034+ThanhthanhQuy@users.noreply.github.com>
2019-04-12 00:06:12 +0700

    bài tập tuần 2

---
+++ function.js
@@ -0,0 +1,18 @@
+function get () {
+    return 'world';
+}
+function post() {
+    return 'world created'
+}
+function put() {
+    return 'world updated'
+}
+function remove() {
+    return 'world deleted';
+}
+exports.get =get;
+exports.post=post;
+exports.put=put;
+exports.remove=remove;
+
+
--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Hello, world!');
+var  http = require('http');
+var url= require('url');
+
+var requestHandle= require('./function');
+
+var data=
+{
+    'GET' : requestHandle.get(),
+    'POST': requestHandle.post(),
+    'PUT' : requestHandle.put(),
+    'DELETE': requestHandle.remove()
+
+}
+
+var server=  http.createServer(function (req, res)  {
+    var message;
+    if(req.url=='/hello') {
+        data[req.method] ? message = data[req.method] : message = 'Invalid';
+        res.write(message);
+        res.end();
+    }
+})
+server.listen(8000, 'localhost', function (err) {
+    if(!err) {console.log('Server is running......')}
+});
