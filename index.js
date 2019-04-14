d61b5dce31e9e79010b85001ed650c50f0f34a1a
ThanhthanhQuy <38715034+ThanhthanhQuy@users.noreply.github.com>
2019-04-14 23:54:24 +0700

    Nộp bài tập tuần 3-UETCODECAMP

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
-var  http = require('http');
-var url= require('url');
+const  http = require('http');
+const url= require('url');
+const host= process.env.HOST;
+const port= process.env.PORT;
 
-var requestHandle= require('./function');
-
-var data=
-{
-    'GET' : requestHandle.get(),
-    'POST': requestHandle.post(),
-    'PUT' : requestHandle.put(),
-    'DELETE': requestHandle.remove()
+function get () {
+    return 'world';
+}
+function post() {
+    return 'world created'
+}
+function put() {
+    return 'world updated'
+}
+function remove() {
+    return 'world deleted';
+}
+function other() {
+    return 'Invalid method';
 
 }
 
+var config = [ {
+    path: '/hello',
+    data:
+        {
+            'GET': get(),
+            'POST': post(),
+            'PUT': put(),
+            'DELETE': remove()
+        }
+}]
+
 var server=  http.createServer(function (req, res)  {
-    var message;
-    if(req.url=='/hello') {
-        data[req.method] ? message = data[req.method] : message = 'Invalid';
+
+    const location = url.parse(req.url);
+    const pathconfig = config.find(function (element) {
+        return (element.path === location.pathname);
+    })
+     const message = pathconfig ?  (pathconfig.data[req.method] || other() ) : 'Invalid';
         res.write(message);
         res.end();
-    }
 })
-server.listen(8000, 'localhost', function (err) {
+server.listen(port, host, function (err) {
     if(!err) {console.log('Server is running......')}
 });
